442ebf22f8318b2079d79963f91a750f6f5cdde8
Natalia55555 <75278309+Natalia55555@users.noreply.github.com>
2021-01-17 18:33:07 +0300

    red err

--- src/components/InputH/InputH.js
+++ src/components/InputH/InputH.js
@@ -1,6 +1,7 @@
 
 import React, {useState} from 'react';
-import { Scene, ConeBufferGeometry, PerspectiveCamera, WebGLRenderer, MeshNormalMaterial, Mesh } from 'three';
+// import { Scene, ConeGeometry, PerspectiveCamera, WebGLRenderer, MeshNormalMaterial, Mesh } from 'three';
+import * as THREE from 'three';
 import './Input.css';
 
 
@@ -15,12 +16,12 @@ const [segments, setSegments] = useState('')
 //   console.log('data', hight, radius )
 // }
 
-const scene = new Scene();
-const geometry = new ConeBufferGeometry( radius, hight, segments );
-const camera = new PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 5000 );
-const material = new MeshNormalMaterial();
+const scene = new THREE.Scene();
+const geometry = new THREE.ConeGeometry( radius, hight, segments );
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 5000 );
+const material = new THREE.MeshNormalMaterial();
 
-const renderer = new WebGLRenderer();
+const renderer = new THREE.WebGLRenderer();
 
 renderer.setSize(800, 500 );
 
@@ -31,7 +32,7 @@ document.body.appendChild( renderer.domElement );
 // renderer.setSize(window.innerWidth, window.innerHeight);
 // document.getElementById('threejs').appendChild(renderer.domElement);
 
-const cone = new Mesh( geometry, material );
+const cone = new THREE.Mesh( geometry, material );
 scene.add( cone );
 
 camera.position.z = 40;
@@ -43,12 +44,11 @@ function render() {
   cone.rotation.z += 0.005;        
   renderer.render( scene, camera );
 }
-render();
+
 
 return(
-  <div>
-  <div className='cone_wrapper' id='cone_wrapper'></div>
-    <form className='form'>        
+  <div className='form_wrap'>  
+    <form >        
           Высота:
             <input 
              type="text"
@@ -69,12 +69,9 @@ return(
              placeholder="segments"
              value={segments}
              onChange={e => setSegments(e.target.value)}              
-              />
-                {/* <button 
-    type='submit'> Отправить
-      </button>                */}
+              />      
     </form> 
-   
+    <button onClick={render} className='btn' >Отрисовать</button>   
 </div>
   );  
 }  
